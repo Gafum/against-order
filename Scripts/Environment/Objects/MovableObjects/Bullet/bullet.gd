082eafe333722844e724a11daeca6488ae8c5a90
Marchuk Dmytro <marcukdyma@gmail.com>
2025-11-25 19:17:02 +0100

    added explodition effect

--- Scripts/Environment/Objects/MovableObjects/Bullet/bullet.gd
+++ Scripts/Environment/Objects/MovableObjects/Bullet/bullet.gd
@@ -7,14 +7,19 @@ extends Area2D
 var direction: Vector2 = Vector2.ZERO
 var is_active: bool = true
 
+var explosion_scene := preload("res://Scripts/Effects/Explosion/explosion.tscn")
+
+
 func _ready() -> void:
 	await get_tree().create_timer(lifetime).timeout
 	if is_active:
 		queue_free()
 
+
 func _physics_process(delta: float) -> void:
 	if not is_active:
 		return
+
 	var velocity = direction * speed + velocity_offset
 	position += velocity * delta
 	rotation = direction.angle()
@@ -26,8 +31,19 @@ func _on_body_entered(body: Node2D) -> void:
 
 	is_active = false
 
+	_spawn_explosion()
+
 	if body.is_in_group("destroyable"):
 		if body.has_method("take_damage"):
 			body.take_damage()
 
 	queue_free()
+
+
+func _spawn_explosion() -> void:
+	if explosion_scene == null:
+		return
+
+	var explosion = explosion_scene.instantiate()
+	explosion.global_position = global_position
+	get_tree().current_scene.add_child(explosion)
