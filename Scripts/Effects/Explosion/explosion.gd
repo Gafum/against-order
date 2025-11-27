591cfb05102f7fcceeaf6a179fd099a5b4acd8ad
Marchuk Dmytro <marcukdyma@gmail.com>
2025-11-27 16:31:36 +0100

    added animation and fixed respawn of effects

--- Scripts/Effects/Explosion/explosion.gd
+++ Scripts/Effects/Explosion/explosion.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+@onready var particle_object = $GPUParticles2D
+
+func _ready():
+	particle_object.emitting = true
+	await get_tree().create_timer(particle_object.lifetime).timeout
+	particle_object.emitting = false
+	particle_object.queue_free()
+	queue_free()
