92c7e0e87c52dc667068ab4fdb7b5175477ac037
Marchuk Dmytro <marcukdyma@gmail.com>
2025-12-06 23:36:23 +0100

    added attack sprites

--- Scripts/Environment/Obstacles/StaticObstacles/Traps/trap_1.gd
+++ Scripts/Environment/Obstacles/StaticObstacles/Traps/trap_1.gd
@@ -2,5 +2,5 @@ extends "res://Scripts/Environment/Obstacles/BaseObstacle/base_obstacle.tres.gd"
 
 @onready var trap_sprite = $AnimatedSprite2D
 
-func _ready() -> void:
+func _ready() -> void: 
 	trap_sprite.frame = randi_range(0, 1)
