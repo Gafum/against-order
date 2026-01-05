aaf5eb7518c496ab498ffff5e0612f90d0c5c767
Marchuk Dmytro <marcukdyma@gmail.com>
2026-01-05 22:54:28 +0100

    redraw some sprites of the bullets

--- Scripts/Environment/Obstacles/StaticObstacles/Traps/trap_1.gd
+++ Scripts/Environment/Obstacles/StaticObstacles/Traps/trap_1.gd
@@ -3,6 +3,5 @@ extends "res://Scripts/Environment/Obstacles/BaseObstacle/base_obstacle.tres.gd"
 @onready var trap_sprite = $AnimatedSprite2D
 
 func _ready() -> void:
-
 	super._ready()
-	trap_sprite.frame = randi_range(0, 2)
+	trap_sprite.frame = randi_range(0, trap_sprite.get_sprite_frames().get_frame_count("default")-1)
