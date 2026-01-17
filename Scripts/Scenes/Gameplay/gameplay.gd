23ca49e4da09a26428ef1e95d47e06a87058d2fb
Marchuk Dmytro <marcukdyma@gmail.com>
2026-01-17 10:13:04 +0100

    code refactoring

--- Scripts/Scenes/Gameplay/gameplay.gd
+++ Scripts/Scenes/Gameplay/gameplay.gd
@@ -7,9 +7,9 @@ const MAX_SPEED := 1700
 @onready var camera: Camera2D = $Camera2D
 @onready var game_floor: StaticBody2D = $Floor
 
-const VILLAIN_LIST := [
+const OBSTAVLES_LIST := [
 	preload("res://Scripts/Environment/Obstacles/StaticObstacles/Liquid/toxic_water.tscn"),
-	preload("res://Scripts/Environment/Obstacles/Villains/Villain1/villain_1.tscn"),
+	preload("res://Scripts/Environment/Obstacles/Villains/BaseVillain/base_villain.tscn"),
 	preload("res://Scripts/Environment/Obstacles/StaticObstacles/Traps/trap1.tscn"),
 	preload("res://Scripts/Environment/Obstacles/StaticObstacles/Blocks/StandartBlock/block.tscn"),
 	preload("res://Scripts/Environment/Obstacles/StaticObstacles/FlyingObstacles/Drons/drone.tscn"),
@@ -25,11 +25,11 @@ var is_game_over: bool = false
 @onready var reload_bar: ProgressBar = %ReloadBar
 @onready var reload_container = $CanvasLayer/RightMarginContainer
 
-var next_villain_x_position: float = 1500.0
+var next_obstacle_x_position: float = 1500.0
 
 func _ready() -> void:
 	Global.is_game_over = false
-	next_villain_x_position = player.global_position.x
+	next_obstacle_x_position = player.global_position.x
 	player.player_died.connect(_on_player_died)
 	player.weapon_reloading.connect(_on_player_weapon_reloading)
 
@@ -52,30 +52,30 @@ func _physics_process(delta: float) -> void:
 	game_floor.global_position.x = player_x + relative_camera_position
 	if (speed < MAX_SPEED):
 		speed += delta * 7 # Increased acceleration slightly as requested ("just increase speed little by little")
-	spawn_villain(player_x)
+	spawn_obstacle(player_x)
 
 
-func spawn_villain(player_x: float):
+func spawn_obstacle(player_x: float):
 	# check if it is enogth space
-	if (player_x < next_villain_x_position):
+	if (player_x < next_obstacle_x_position):
 		return
 		
-	# set the next position of the villain
-	next_villain_x_position = int(player_x + max(720, speed / 2) + randi_range(0, 200))
+	# set the next position of the obstacle
+	next_obstacle_x_position = int(player_x + max(720, speed / 2) + randi_range(0, 200))
 	
-	var new_villain_object = VILLAIN_LIST[randi_range(0, VILLAIN_LIST.size() - 1)]
+	var new_obstacle_object = OBSTAVLES_LIST[randi_range(0, OBSTAVLES_LIST.size() - 1)]
 	
-	if (new_villain_object):
-		# creating the new Villain
-		var new_villain: CharacterBody2D = new_villain_object.instantiate()
-		new_villain.name = "VILLAIN" + str(Time.get_ticks_msec())
+	if (new_obstacle_object):
+		# creating the new obstacle
+		var new_obstacle: CharacterBody2D = new_obstacle_object.instantiate()
+		new_obstacle.name = "Obstacle" + str(Time.get_ticks_msec())
 		
 		# Always spawn at a fixed distance from the player, regardless of screen resolution
 		var spawn_x = player_x + 1500 + speed
 		
-		new_villain.global_position = Vector2(spawn_x, VILLAIN_Y)
-		new_villain.scale = Vector2(0.7, 0.7)
-		add_child(new_villain)
+		new_obstacle.global_position = Vector2(spawn_x, VILLAIN_Y)
+		new_obstacle.scale = Vector2(0.7, 0.7)
+		add_child(new_obstacle)
 
 
 func _on_player_died():
