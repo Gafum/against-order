0ae6699f35e4a21c11a326ffd8424bed52618da2
Marchuk Dmytro <marcukdyma@gmail.com>
2026-01-25 20:16:07 +0100

    added trash sprite and fixed standart block

---
+++ Scripts/Environment/Obstacles/StaticObstacles/Blocks/Barrel/barrel.gd
@@ -0,0 +1,20 @@
+extends "res://Scripts/Environment/Obstacles/StaticObstacles/Blocks/StandartBlock/block.gd"
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	super._ready()
+	name = "Barrel" + str(Time.get_ticks_msec())
+	
+	var animation_names := animated_sprite_2d.sprite_frames.get_animation_names()
+	
+	if (!len(animation_names)):
+		return
+	
+	var random_ani_name = animation_names[randi() % animation_names.size()]
+	if(random_ani_name == "box" || random_ani_name == "barrel"):
+		random_ani_name = "barrel"
+	else:
+		self.scale = Vector2(0.6, 0.6)
+		
+	animated_sprite_2d.play(random_ani_name)
